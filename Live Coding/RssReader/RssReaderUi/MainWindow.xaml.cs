0f78fec1732d217436fc036cca1566e81156713c
Olaf Lischke <olaf.lischke@lischke-edv.de>
2024-11-18 17:26:07 +0100

    Tag 1

--- Live Coding/RssReader/RssReaderUi/MainWindow.xaml.cs
+++ Live Coding/RssReader/RssReaderUi/MainWindow.xaml.cs
@@ -1,34 +1,24 @@
 ﻿using System.ServiceModel.Syndication;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml;
 
-namespace RssReaderUi
+namespace RssReaderUi;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            string url = "https://www.spiegel.de/schlagzeilen/index.rss";
+        string url = "https://www.spiegel.de/schlagzeilen/index.rss";
 
-            XmlReader xmlReader = XmlReader.Create(url);
-            SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+        XmlReader xmlReader = XmlReader.Create(url);
+        SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
 
-            // Basis aller Binding-Ausdrücke
-            this.DataContext = feed;
-        }
+        // Basis aller Binding-Ausdrücke
+        this.DataContext = feed;
     }
 }
